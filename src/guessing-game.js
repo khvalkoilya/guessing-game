1cad1bb8a77e2a4bf7f9f6b6dc64e6281659a02f
Khvalkoilya <khvalko.ilya@gmail.com>
2019-10-08 21:28:57 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let a;
+        let b;
+        let mid_number;
+    }
+
 
     setRange(min, max) {
-
+        this.a = min;
+        this.b = max;
     }
 
     guess() {
-
+        this.mid_number = Math.ceil((this.a+this.b)/2);
+        return this.mid_number;
     }
 
     lower() {
-
+        this.setRange(this.a, this.mid_number);
     }
 
     greater() {
-
+        this.setRange(this.mid_number, this.b);
     }
 }
 
